4266f73f01c35bd0e2489b2b054cf060be3e1313
Tushar Nankani <tusharnankani3@gmail.com>
2023-03-28 11:49:06 +0530

    changes to client and server for gc

--- 02_gc_socket/client.py
+++ 02_gc_socket/client.py
@@ -1,13 +1,16 @@
 import socket
 
-HOST = '192.168.1.177'
+HOST = '192.168.1.161'
 PORT = 12345
 
 s = socket.socket()
 s.connect((HOST, PORT))
-res = s.recv(1024).decode()
-print(res)
 
-s.sendall(input("Enter Message: ").encode("utf-8"))
+while True:
+	res = s.recv(1024).decode()
+	if res is None:
+	    continue
+	print(res)
+	s.sendall(input("Enter Message: ").encode("utf-8"))
 
 s.close()
--- 02_gc_socket/server.py
+++ 02_gc_socket/server.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket()
-print ("Socket successfully created")
+print ("socket successfully created")
 
 port = 12345
 
@@ -11,15 +11,25 @@ print ("socket binded to %s" %(port))
 s.listen(5)
 print ("socket is listening")
 
+connections = []
+
 while True:
 
     c, addr = s.accept()
     print ('Got connection from', addr )
-
-    c.send('Thank you for connecting with Yohan server'.encode())
-
-    print(c.recv(1024).decode())
-
+    c.send('Thank you for connecting with the server'.encode())
+    
+    connections.append((c, addr))
+    msg = c.recv(1024).decode()
+    
+    for (c, addr) in connections:
+    	c.send(f"Message from {addr}: {msg}".encode())
+    
+    if len(connections) > 2:
+    	break
+    
+for (c, addr) in connections:
+    print(f"Closing ({addr})")
     c.close()
 
-
+s.close()
